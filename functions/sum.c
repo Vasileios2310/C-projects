ab8b1c291546577337bfef942db3b5cecca0b619
Vasileios2310 <vasileioskritikos@aueb.gr>
2026-01-29 18:31:08 +0200

    sum function

---
+++ functions/sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum(int a , int b);
+
+int main (void)
+{
+    int total;
+
+    total = sum(5,6);
+    printf("The sum is : %d\n" , total);
+
+    return 0;
+}
+
+int sum(int a , int b)
+{
+    int s;
+    return s = a + b;
+
+}
